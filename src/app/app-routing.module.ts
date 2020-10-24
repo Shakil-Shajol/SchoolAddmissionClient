8a82f0713bce68cecfb7c8f1cee80cda8755fd3d
Shakil-Shajol <shakil.bd.ngn@gmail.com>
2020-10-24 08:40:15 +0600

    commit try

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddmissionComponent } from './addmission/addmission.component';
+import { ApplicationComponent } from './addmission/application/application.component';
+import { HomeComponent } from './home/home.component';
+import { AddNoticeComponent } from './notice/add-notice/add-notice.component';
+import { NoticeComponent } from './notice/notice.component';
+import { AddSessionComponent } from './session/add-session/add-session.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'notice',component:NoticeComponent},
+  {path:'addmission',component:AddmissionComponent},
+  {path:'apply',component:ApplicationComponent},
+  {path:'notice/add',component:AddNoticeComponent},
+  {path:'session/add',component:AddSessionComponent},
+  {path:'notice/:id',component:NoticeComponent},
+  {path:'**',component:HomeComponent,pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MatSharedModule } from './mat-shared/mat-shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NoticeComponent } from './notice/notice.component';
+import { NoticeService } from "./services/notice.service";
+import { HttpClientModule } from '@angular/common/http';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
+import { FooterComponent } from './footer/footer.component';
+import { AddNoticeComponent } from './notice/add-notice/add-notice.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { AddmissionComponent } from './addmission/addmission.component';
+import { ApplicationComponent } from './addmission/application/application.component';
+import { ApplicationService } from "./services/application.service";
+import { ExamService } from "./services/exam.service";
+import { SessionService } from "./services/session.service";
+import { SessionComponent } from './session/session.component';
+import { AddSessionComponent } from './session/add-session/add-session.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    NoticeComponent,
+    FooterComponent,
+    AddNoticeComponent,
+    AddmissionComponent,
+    ApplicationComponent,
+    SessionComponent,
+    AddSessionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSharedModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatCarouselModule.forRoot(),
+    FormsModule,
+    ToastrModule.forRoot(),
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [NoticeService,ApplicationService,ExamService,SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
